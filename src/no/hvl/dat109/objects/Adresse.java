444bb4fa6707fa6f2b6d01e04fd5c4c4be31163e
GlennBarosen <glennbarosen@gmail.com>
2020-02-11 10:22:24 +0100

    lagt til adresse 🏠

---
+++ src/no/hvl/dat109/objects/Adresse.java
@@ -0,0 +1,32 @@
+package no.hvl.dat109.objects;
+
+public class Adresse {
+	
+	private String gateadresse;
+	private int postnr;
+	private String poststed;
+	
+	public Adresse(String gateadresse, int postnr, String poststed) {
+		super();
+		this.gateadresse = gateadresse;
+		this.postnr = postnr;
+		this.poststed = poststed;
+	}
+
+	public String getGateadresse() {
+		return gateadresse;
+	}
+
+	public int getPostnr() {
+		return postnr;
+	}
+
+	public String getPoststed() {
+		return poststed;
+	}
+	
+	
+	
+	
+
+}
