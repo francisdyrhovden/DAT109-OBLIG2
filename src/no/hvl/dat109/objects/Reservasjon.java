a6c14ecadd1155b29cf5f0ccb62e3d2ff80addee
Francis Soliman <soliman_61@hotmail.com>
2020-02-11 12:24:38 +0100

    La til kunde i reservasjon

--- src/no/hvl/dat109/objects/Reservasjon.java
+++ src/no/hvl/dat109/objects/Reservasjon.java
@@ -16,19 +16,47 @@ public class Reservasjon {
 	private int antallDager;
 	private Kontor utleiested;
 	private Kontor retursted;
+	private Kunde kunde;
 	
 	
-	public Reservasjon(Bil bil, Date startDato, int antallDager, Kontor utleiested, Kontor retursted) {
+	public Reservasjon(Bil bil, Date startDato, int antallDager, Kontor utleiested, Kontor retursted, Kunde kunde) {
 		super();
 		this.bil = bil;
 		this.startDato = startDato;
 		this.antallDager = antallDager;
 		this.utleiested = utleiested;
 		this.retursted = retursted;
+		this.kunde = kunde;
 	}
 	
 	public void reserver(Bil bil) {
 		bil.setLedig(false);
 	}
+
+	public Bil getBil() {
+		return bil;
+	}
+
+	public Date getStartDato() {
+		return startDato;
+	}
+
+	public int getAntallDager() {
+		return antallDager;
+	}
+
+	public Kontor getUtleiested() {
+		return utleiested;
+	}
+
+	public Kontor getRetursted() {
+		return retursted;
+	}
+
+	public Kunde getKunde() {
+		return kunde;
+	}
+	
+	
 	
 }
