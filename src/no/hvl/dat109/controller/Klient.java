ad2ae04322e9d9a226d65a218571619dd18b4e92
GlennBarosen <glennbarosen@gmail.com>
2020-02-20 13:54:51 +0100

    små endringer 🥉

--- src/no/hvl/dat109/controller/Klient.java
+++ src/no/hvl/dat109/controller/Klient.java
@@ -6,9 +6,18 @@ public class Klient {
 
 	public static void main(String[] args) {
 		
-		//Test om enum funker :))))
-		Bil b1 = new Bil("EV92354", "Tesla", "Model 3", "Svart", UtleiegruppeEnum.STOR_BIL);
-		System.out.println(b1.toString());
+		Selskap easyCar = new Selskap("EasyCar", new Adresse("tulleveien 1", 4308, "Sandnes"), "92822779");
+		
+		Bilpark bilpark = new Bilpark();
+		
+		Kontor sandnes = new Kontor("Sandnes", new Adresse("Tulleveien 1", 4308, "Sandnes"), easyCar);
+		Kontor bergen = new Kontor("Bergen", new Adresse("Tulleveien 2", 5063, "Bergen"), easyCar);
+		
+		bergen.setBiler(bilpark.leggTilBiler1());
+		sandnes.setBiler(bilpark.leggTilBiler2());
+		
+		SokEtterBil bilSok = new SokEtterBil();
+		bilSok.sokBil(easyCar);
 
 	}
 
